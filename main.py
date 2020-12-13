9acbf4134f1614e67882cebe2d65d5ca926c9db4
Max Hauer <github@mhauer.de>
2020-12-13 20:42:29 +0100

    v0.4

--- main.py
+++ main.py
@@ -14,22 +14,43 @@ class MainApp:
         self.frame = tk.Frame(self.parent)
         # create the parent window
         self.parent.title("Worktimer")
+        self.parent.geometry("200x150")
+
         # createsub frames
         self.f_Input = tk.Frame(self.parent)
         self.f_Input.grid(row=0, column=0)
         self.f_Button = tk.Frame(self.parent)
         self.f_Button.grid(row=1, column=0)
+        # TimeperWeek label
+        self.l_TimePerWeek = tk.Label(self.f_Input, text="Arbeitszeit/Woche:")
+        self.l_TimePerWeek.grid(row=0, column=0)
+        # TimeperWeek Inputfield
+        self.varTimeWeek = tk.StringVar()
+        # self.varTimeWeek.set("35")
+        workweeksetting = loadSetting("workweekhours")
+        self.varTimeWeek.set(workweeksetting[0])
+        self.e_TimePerWeek = tk.Entry(
+            self.f_Input, textvariable=self.varTimeWeek, width=10
+        )
+        self.e_TimePerWeek.grid(row=0, column=1)
+        # TimeperWeek Button
+        self.b_SaveTimeWeek = tk.Button(
+            self.f_Input,
+            text="Save",
+            command=lambda: insertWeekhours(self.varTimeWeek.get()),
+        )
+        self.b_SaveTimeWeek.grid(row=0, column=3)
         # Date Label
-        self.l_Date = tk.Label(self.f_Input, text="Date:")
-        self.l_Date.grid(row=0, column=0)
+        self.l_Date = tk.Label(self.f_Input, text="Datum:")
+        self.l_Date.grid(row=1, column=0)
         # Date Inputfield
         self.varDate = tk.StringVar()
         self.varDate.set(getCurrDate())
         self.e_Date = tk.Entry(self.f_Input, textvariable=self.varDate, width=10)
-        self.e_Date.grid(row=0, column=1)
+        self.e_Date.grid(row=1, column=1)
         # Time Label
-        self.l_Time = tk.Label(self.f_Input, text="Time:")
-        self.l_Time.grid(row=1, column=0)
+        self.l_Time = tk.Label(self.f_Input, text="Uhrzeit:")
+        self.l_Time.grid(row=2, column=0)
         # Time Inputfield
         self.varTime = tk.StringVar()
         self.varTime.set(getCurrTime())
@@ -40,7 +61,20 @@ class MainApp:
                 self.e_Time
             ),
         )
-        self.e_Time.grid(row=1, column=1)
+        self.e_Time.grid(row=2, column=1)
+        # Break label
+        self.l_Break = tk.Label(self.f_Input, text="Pause:")
+        self.l_Break.grid(row=3, column=0)
+        # Break Radiobutton
+        self.varBreak = tk.IntVar()
+        self.b_BreakYes = tk.Radiobutton(
+            self.f_Input, text="Ja", variable=self.varBreak, value=1
+        )
+        self.b_BreakYes.grid(row=4, column=0)
+        self.b_BreakNo = tk.Radiobutton(
+            self.f_Input, text="Nein", variable=self.varBreak, value=0
+        )
+        self.b_BreakNo.grid(row=4, column=1)
         # Button Kommen
         self.b_Kommen = tk.Button(
             self.f_Button,
@@ -52,7 +86,9 @@ class MainApp:
         self.b_Gehen = tk.Button(
             self.f_Button,
             text="Gehen",
-            command=lambda: saveGehen(self.varDate.get(), self.varTime.get()),
+            command=lambda: saveGehen(
+                self.varDate.get(), self.varTime.get(), self.varBreak.get()
+            ),
         )
         self.b_Gehen.grid(row=0, column=1)
 
@@ -83,6 +119,17 @@ def createDB():
             UEBERSTUNDEN TEXT
         )"""
         )
+        cur.execute(
+            """
+        CREATE TABLE IF NOT EXISTS settings (
+            NAME TEXT,
+            VALUE NTEGER 
+        )"""
+        )
+        cur.execute(
+            "INSERT INTO settings(NAME,VALUE) VALUES(?,?)", ("workweekhours", 35)
+        )
+        con.commit()
         cur.close()
     except sqlite3.Error as error:
         print("Failed to create database", error)
@@ -135,7 +182,7 @@ def saveKommen(dat, tim):
             con.close()
 
 
-def saveGehen(dat, tim):
+def saveGehen(dat, tim, didbreak):
     try:
         con = db_connect()
         cur = con.cursor()
@@ -147,10 +194,39 @@ def saveGehen(dat, tim):
     finally:
         if con:
             con.close()
-        calcWorktime(dat)  # call calcWorktime
+        insertBreak(dat, didbreak)
 
 
-def calcWorktime(date):
+def insertBreak(dat, didbreak):
+    try:
+        con = db_connect()
+        cur = con.cursor()
+        cur.execute("UPDATE worktime SET PAUSE=? WHERE DATUM=?", (didbreak, dat))
+        con.commit()
+        cur.close()
+    except sqlite3.Error as error:
+        print("Failed to update PAUSE", error)
+    finally:
+        con.close()
+        calcWorktime(dat, didbreak)  # call calcWorktime
+
+
+def insertWeekhours(weekhours):
+    try:
+        con = db_connect()
+        cur = con.cursor()
+        cur.execute(
+            "UPDATE settings SET VALUE=? WHERE NAME=?",
+            (weekhours, "workweekhours"),
+        )
+        cur.close()
+    except sqlite3.Error as error:
+        print("Inserting of Weekhours did not work", error)
+    finally:
+        con.close()
+
+
+def calcWorktime(date, didbreak):
     try:
         con = db_connect()
         cur = con.cursor()
@@ -165,6 +241,38 @@ def calcWorktime(date):
         delta = dt.datetime.strptime(select_bis[0], "%H:%M") - dt.datetime.strptime(
             select_von[0], "%H:%M"
         )
+        if didbreak == 1:
+            if delta >= dt.timedelta(hours=8):
+                delta = delta - dt.timedelta(hours=1)
+            elif delta <= dt.timedelta(hours=8) and delta >= dt.timedelta(hours=5):
+                delta = delta - dt.timedelta(hours=0, minutes=30)
+
+        strdelta = str(delta)
+
+        cur.execute("UPDATE worktime SET ARBEITSZEIT=? WHERE DATUM=?", (strdelta, date))
+        con.commit()
+        cur.close()
+
+    except sqlite3.Error as error:
+        print("Calculation of Worktime was not possible", error)
+    finally:
+        con.close()
+        CalcOvertime(date)
+
+
+def CalcOvertime(date):
+    try:
+        con = db_connect()
+        cur = con.cursor()
+        cur.execute("SELECT VALUE FROM settings WHERE NAME=?", "workweekhours")
+        weektime = cur.fetchone()
+        timePerDay = weektime / 5
+        cur.execute("SELECT ARBEITSZEIT FROM worktime WHERE DATUM=?", [date])
+        select_arbeitszeit = cur.fetchone()
+        delta = dt.datetime.strptime(
+            select_arbeitszeit[0], "%H:%M"
+        ) - dt.datetime.strptime(timePerDay)
+
         strdelta = str(delta)
         cur.execute("UPDATE worktime SET ARBEITSZEIT=? WHERE DATUM=?", (strdelta, date))
         con.commit()
@@ -172,6 +280,20 @@ def calcWorktime(date):
 
     except sqlite3.Error as error:
         print("Calculation of Worktime was not possible", error)
+    finally:
+        con.close()
+
+
+def loadSetting(name):
+    try:
+        con = db_connect()
+        cur = con.cursor()
+        cur.execute("SELECT VALUE FROM settings WHERE NAME=?", [name])
+        value = cur.fetchone()
+        cur.close()
+        return value
+    except sqlite3.Error as error:
+        print("Loading from Setting was not possible: ", error)
     finally:
         con.close()
 
